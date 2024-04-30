349923a86429c192234aae4cf43e8840c4f0a656
dsanguti <dsanguti@gmail.com>
2024-04-30 14:21:49 +0200

    componenetes botones y contador. Falta funcionalidad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
+import Boton from "./components/Boton.jsx";
+import Contador from "./components/Contador.jsx";
+import logo from "./img/freecodecamp-logo.svg";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+
+  const [numClics, setNumClics]
+
+  const manejarClic = () => {
+    console.log("Hola soy ManjerClic");
+  };
+
+  const reiniciarContador = () => {
+    console.log("Hola soy reiniciarContador");
+  };
+
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <h4 id="titulo-logo">freeCodeCamp</h4>
+        <img className="freecodecamp-logo" alt="logo-freecodecamp" src={logo} />
+      </div>
+      <div className="contenedor-principal">
+        <Contador numClics="2" />
+        <Boton texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
